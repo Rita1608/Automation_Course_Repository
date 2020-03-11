8994d932c6769b9a359867e8bd301bff19dc5d15
Rita Tukach <rita.tukach@idt.net>
2020-03-11 19:59:42 +0300

    HT2 L5

---
+++ src/main/java/Lesson5/MyMainClass.java
@@ -0,0 +1,50 @@
+package Lesson5;
+
+public class MyMainClass<symbol> {
+    public static void main(String[] args) {
+        //String myName = "My name is Rita";
+        //printBeautifully(myName, '$');
+
+        //}
+
+        //public static void printBeautifully(String smthToDisplay, char symbol){
+        //System.out.println(symbol + "     " + symbol);
+        // System.out.println(smthToDisplay);
+        //System.out.println(symbol + "    " + symbol);
+        //}
+//}
+
+        //String catName = "Vasya";
+        //String dogName = "Zhuchka";
+        //displayAnimals(catName);
+
+        //public static void methodB(displayAnimals, String space) {
+        //System.out.println(displayAnimals);
+        //System.out.println(space);
+        //}
+
+        //String myString = combinedString(String a, String b, char c);
+        //printMystring(String myString);
+
+        String myString;
+        myString = combinedString("Belka", "Luche Strelki", '-');
+        printMyString(myString);
+    }
+
+        public static String combinedString(String a, String b, char c){
+            String myCombinedString;
+            myCombinedString = a + c + b;
+            return myCombinedString;
+
+
+        }
+
+        public static void printMyString(String myString){
+            System.out.println(myString);
+        }
+
+    }
+
+
+
+
